5e73b6cc8abde9895ca64fff13adc26ad24f8bb3
Macchi <renanmacchi@hotmail.com>
2024-01-01 21:38:25 -0300

    Created menu overlay function over page content

--- static/menu.js
+++ static/menu.js
@@ -3,7 +3,15 @@ document.addEventListener('DOMContentLoaded', function () {
     const mainMenu = document.getElementById('main-menu');
 
     menuHeader.addEventListener('click', function () {
-        mainMenu.style.display = (mainMenu.style.display === 'block') ? 'none' : 'block';
+        mainMenu.style.display = mainMenu.style.display === 'block' ? 'none' : 'block';
         menuHeader.classList.toggle('menu-opened');
+    });
+
+    // Fechar o menu se clicar fora dele
+    document.addEventListener('click', function (event) {
+        if (!menuHeader.contains(event.target) && !mainMenu.contains(event.target)) {
+            mainMenu.style.display = 'none';
+            menuHeader.classList.remove('menu-opened');
+        }
     });
 });
